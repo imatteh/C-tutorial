b612a3a4c341fa75c3c7b35456227e214622f069
imatteh <imatteh@oyia.net>
2017-12-23 18:33:24 +0200

    First

---
+++ Start With C-sharp/Start With C-sharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello Wordl ");
+        string NameSpaeb = Console.ReadLine();
+        Console.WriteLine("Hello" + NameSpaeb);
+
+
+    }
+}
